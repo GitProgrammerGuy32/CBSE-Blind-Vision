06dc3a3420ab596e2f394a3355e93b0508212df5
GitProgrammerGuy32 <141158070+GitProgrammerGuy32@users.noreply.github.com>
2023-12-15 22:32:34 +0530

    Add files via upload

---
+++ ESP 32 CAM V1/betapp.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, Response
+import cv2
+import urllib.request
+import numpy as np
+
+app = Flask(__name__)
+
+# This is to pull the information about what each object is called
+classNames = []
+classFile = "Weights/coco.names"
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+# This is to pull the information about what each object should look like
+configPath = "Weights/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "Weights/frozen_inference_graph.pb"
+
+# This is some set up values to get good results
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+# This is to set up what the drawn box size/colour is and the font/size/colour of the name tag and confidence label
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img, confThreshold=thres, nmsThreshold=nms)
+    if len(objects) == 0:
+        objects = classNames
+    objectInfo = []
+    if len(classIds) != 0:
+        for classId, confidence, box in zip(classIds.flatten(), confs.flatten(), bbox):
+            className = classNames[classId - 1]
+            if className in objects:
+                objectInfo.append([box, className])
+                if draw:
+                    cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
+                    cv2.putText(img, classNames[classId - 1].upper(), (box[0] + 10, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(img, str(round(confidence * 100, 2)), (box[0] + 200, box[1] + 30),
+                                cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    return img, objectInfo
+
+def generate():
+    while True:
+        img_resp = urllib.request.urlopen(url)
+        imgnp = np.array(bytearray(img_resp.read()), dtype=np.uint8)
+        img = cv2.imdecode(imgnp, -1)
+
+        result, _ = getObjects(img, 0.60, 0.2)
+        _, jpeg = cv2.imencode('.jpg', img)
+        frame = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video')
+def video():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    # Replace this URL with the actual URL of your ESP32 cam
+    url = 'http://192.168.1.8/cam-hi.jpg'
+   
+    app.run(debug=True)
