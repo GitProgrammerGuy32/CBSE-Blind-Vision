9ed21a500dcbef81853dc43a9ac839db70d14eb5
GitProgrammerGuy32 <141158070+GitProgrammerGuy32@users.noreply.github.com>
2023-12-27 18:01:12 +0530

    Create pro_imagecaptioning.py

---
+++ Raspberry Pi/pro_imagecaptioning.py
@@ -0,0 +1,66 @@
+import google.generativeai as genai
+from pathlib import Path
+import pyttsx3
+
+genai.configure(api_key='AIzaSyB-IbJ3LvK2x10tWXxcDcQv_2CRoYBLPQI')
+
+generation_config = {
+  "temperature": 0.5,
+  "top_p": 1,
+  "top_k": 32,
+  "max_output_tokens": 4096,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  }
+]
+
+model = genai.GenerativeModel(model_name = "gemini-pro-vision",
+                              generation_config = generation_config,
+                              safety_settings = safety_settings)
+
+
+def input_image_setup(file_loc):
+    if not (img := Path(file_loc)).exists():
+        raise FileNotFoundError(f"Could not find image: {img}")
+
+    image_parts = [
+        {
+            "mime_type": "image/jpeg",
+            "data": Path(file_loc).read_bytes()
+            }
+        ]
+    return image_parts
+
+def generate_gemini_response(input_prompt, image_loc, question_prompt):
+
+    image_prompt = input_image_setup(image_loc)
+    prompt_parts = [input_prompt, image_prompt[0], question_prompt]
+    response = model.generate_content(prompt_parts)
+    return response.text
+
+input_prompt = """"
+               You are an expert in understanding scenarios and reading texts.
+               You will receive input images as scenarios &
+               you will have to describe scenarios and identify objects and read texts based on the input image
+               """
+
+image_loc = "Videos/railway.jpeg"
+question_prompt = "How many luggages does the guy infront of me has?"
+response = generate_gemini_response(input_prompt, image_loc, question_prompt)
+print(response)
