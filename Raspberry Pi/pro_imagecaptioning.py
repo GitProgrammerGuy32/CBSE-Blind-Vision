fed3a18cce8aac9720ffb9915141a9e81b69994c
GitProgrammerGuy32 <141158070+GitProgrammerGuy32@users.noreply.github.com>
2024-01-25 15:17:00 +0530

    Update pro_imagecaptioning.py

--- Raspberry Pi/pro_imagecaptioning.py
+++ Raspberry Pi/pro_imagecaptioning.py
@@ -5,19 +5,23 @@ import threading
 import os
 import atexit
 import pyttsx3
+import RPi.GPIO as GPIO
 
 engine = pyttsx3.init()
+
 # Initialize webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 640)
+
 genai.configure(api_key='AIzaSyB-IbJ3LvK2x10tWXxcDcQv_2CRoYBLPQI')
 
 # Initialize counter
 counter = 0
+image_captured = False  # Flag to track whether an image has been captured
 
 generation_config = {
-    "temperature": 0.7	,
+    "temperature": 0.7,
     "top_p": 1,
     "top_k": 32,
     "max_output_tokens": 4096,
@@ -108,6 +112,11 @@ def delete_images_in_folder(folder_path):
     except Exception as e:
         print(f"Error deleting images: {e}")
 
+# GPIO setup
+button_pin = 3  # Replace with the GPIO pin connected to your button
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 # Replace 'your_folder_path' with the actual path of the folder containing images
 folder_path = 'Videos'
 
@@ -118,9 +127,8 @@ while True:
     # Display the resulting frame
     cv2.imshow('Webcam Feed', frame)
 
-    # Check for the 's' key press
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('s'):
+    # Check for GPIO pin press
+    if GPIO.input(button_pin) == GPIO.LOW and not image_captured:
         # Increment counter
         counter += 1
 
@@ -134,9 +142,16 @@ while True:
         threading.Thread(target=generate_gemini_response_async, args=(input_prompt, image_loc, question_prompt)).start()
 
         print(f"Image {counter} saved as {filename}")
+        
+        image_captured = True  # Set the flag to True
+
+    # Reset the flag if the button is released
+    if GPIO.input(button_pin) == GPIO.HIGH:
+        image_captured = False
 
     # Check for the 'q' key press to exit the loop
-    elif key == ord('q'):
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
         delete_images_in_folder(folder_path)
         break
 
